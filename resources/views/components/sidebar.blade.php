f154bacbf54fd9452fd98d1614ee0c08d56bbd0f
fasyadita <fasyadita1@gmail.com>
2025-05-26 18:44:15 +0700

    perbaikan tampilan + logo ts

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -1,6 +1,7 @@
 <div class="main-sidebar sidebar-style-2">
     <aside id="sidebar-wrapper">
         <div class="sidebar-brand">
+            <img src="{{ asset('img/logo.png') }}" width="30px" height="30px" alt="">
             <a href="index.html">Tracer Study</a>
         </div>
         <div class="sidebar-brand sidebar-brand-sm">
